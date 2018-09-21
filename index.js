cd4feae2e1d8d9e7c78624a2385a81a0b1fb2fda
dsevastis <dsevastis@gamingsynergies.com>
2018-09-21 12:35:40 +0300

    api implementation

--- index.js
+++ index.js
@@ -1,125 +1,20 @@
+﻿const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
 
-mongoose.connect('mongodb://localhost/playground').then(() => {
-	console.log('Connected to mongoDb')
-}).catch((err) => { console.erron(err) }); //connect to db
-
-const courseSchema = new mongoose.Schema(  //db.schema
-	{
-		name: String,
-		author: String,
-		tags: [String],
-		date: { type: Date, default: Date.now },
-		isPublished: Boolean
-	}
-);
-
-const Course = mongoose.model('Course', courseSchema); //class -- Course is the collection courses in singular
-
-async function createCourse() {
-	const course = new Course({   //object
-		name: 'Angular Course',
-		author: 'Mosh',
-		tags: ['angular', 'frontend'],
-		isPublished: true
+mongoose.connect('mongodb://localhost/mongo-exercises')
+	.then(function () {
+		console.log('Connected to MongoDb...');
+	})
+	.catch(function () {
+		console.log('Failed to connect to MongoDb...');
 	});
 
-	const result = await course.save();
+app.use(express.json());
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
 
-	console.log(result);
-}
-   
-async function getCourses() {
-
-	//Query Operators
-	//----------------------------------------
-	// eq (equal)    
-	//ne(not equal)
-	//gt	(greater than) 
-	//gte	(greater than or equal)
-	//lt (less than)
-	//lte (less than or equal)
-	//in 
-	//nin (not in)
-
-	//======================================
-
-	//Logical Operators
-	//--------------------------
-	//or
-	//and
-
-
-	const pageNumber = 2;
-	const pageSize = 10;
-	//.find({ author: 'Mosh', isPublished: true })
-
-	//QUERY
-	//.find({ price: { $gt: 10, $lte: 20 } })    // .net -> where(x=>x.price > 10 && x.price < 20)
-	//.find({ price: { $in: [10, 15, 20] } })    // .net -> where(x=>x.price == 10 || x.price == 15 || x.price == 20)
-
-
-	//LOGICAL
-	//.find()
-	//.or([{ author: 'Mosh' }, { isPublished: true }])
-	//.and([{}])
-
-	const courses = Course
-		.find({ author: 'Mosh', isPublished: true })
-
-
-		.skip((pageNumber - 1) * pageSize) //pagination
-		.limit(pageSize)
-
-
-		.sort({ name: 1 }) //asc = 1, desc = -1 by name
-		.select({ name: 1, tags: 1 })
-		//.count() 
-		.then((res) => {
-			console.log(res)
-		});
-}
-
-async function updateCourseQueryFirst(id) {
-	//Approach : Query first
-	//findById
-	//modify ,save
-
-	const course = await Course.findById(id);
-
-	if (!course) return;
-
-	course.isPublished = true;  //option 1
-	course.author = 'Mitsos2';
-
-	//course.set({							//option 2
-	//	isPublished: true,
-	//	author: 'Mitsos'
-	//});
-
-	const result = await course.save();
-	console.log(result);
-}
-
-async function updateCourseUpdateFirst(id) {
-	//Approach : Update first
-
-	const course = await Course.update({ _id: id }, {
-		$set: {
-			author: 'Mosh',
-			isPublished: false
-		}
-	});
-
-	console.log(course);
-}
-
-async function removeCourse(id) {
-	//const result = await Course.deleteOne({ _id: id });
-	//const result = await Course.deleteMany({ _id: id });
-	const result = await  Course.findByIdAndRemove(id);
-
-	console.log(result);
-}
-
-//test home
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ models/customer.js
@@ -0,0 +1,35 @@
+﻿const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const Customer = mongoose.model('Customer', new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			minlength: 5,
+			maxlength: 50
+		},
+		isGold: {
+			Boolean,
+			default:false
+		},
+		phone: {
+			type: Number,
+			min: 5,
+			max: 10
+		}
+	}
+));
+
+function validateCustomer(genre) {
+	const schema = {
+		name: Joi.string().min(5).max(50).required(),
+		phone: Joi.string().min(5).max(50).required()(),
+		isGold: Joi.boolean()
+	};
+
+	return Joi.validate(genre, schema);
+}
+
+exports.Customer = Customer;
+exports.validate = validateCustomer;
