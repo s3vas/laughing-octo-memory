aa0f6373d30ca8fda505d29a61efe46a6647e7d4
dsevastis <dsevastis@gamingsynergies.com>
2018-09-27 15:47:55 +0300

    Modeling relationships between connected data

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-﻿const genres = require('./routes/genres');
+﻿const Joi = require('Joi')
+Joi.objectId = require('joi-objectid')(Joi);
+
+const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
+
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
@@ -15,6 +21,8 @@ mongoose.connect('mongodb://localhost/mongo-exercises')
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
